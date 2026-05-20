cf517176d45b1604e990f8eb1e3c2673c7f0796f
carlosnainggolan <145370056+carlosnainggolan@users.noreply.github.com>
2026-05-20 13:14:36 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let data = [2, 4, 6, 8];
+let max = data[0];
+let min = data[0];
+let avg = 0;
+
+for (let i = 0; i < data.length; i++) {
+    if (data[i] > max) {
+        max = data[i];
+    }
+
+    if (min > data[i]) {
+        min = data[i];
+    }
+
+    avg += data[i];
+}
+avg /= data.length;
+
+console.log(`Nilai Avg:  ${avg}`);
+console.log(`Nilai Min: ${min}`);
+console.log(`Nilai Max: ${max}`);
